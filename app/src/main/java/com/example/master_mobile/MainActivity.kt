2ad1fd1641449c7da7515c965554e0dadbe00734
Maja Testad <mytestad@uio.no>
2023-12-08 08:07:55 +0100

    create map with position singapore

--- app/src/main/java/com/example/master_mobile/MainActivity.kt
+++ app/src/main/java/com/example/master_mobile/MainActivity.kt
@@ -4,12 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.master_mobile.ui.theme.MastermobileTheme
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
